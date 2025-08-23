946ec6f6079f9b753020181b609fa6413d2dd535
Liubomyr <guyfloki@gmail.com>
2025-08-23 13:37:18 +0200

    Update iir_cuda_kernel.h

--- flashlfilter/iir_cuda_kernel.h
+++ flashlfilter/iir_cuda_kernel.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <torch/extension.h>
 
-void cuda_lfilter_core_loop_chunked(
-    const at::Tensor& input, const at::Tensor& a_flipped,
-    at::Tensor& output, int64_t chunk_size);
+void cuda_lfilter_fused_persistent(
+    const at::Tensor& x,            // [B,C,N], CUDA
+    const at::Tensor& a_norm,       // [C,K], CUDA
+    const at::Tensor& b_norm,       // [C,K], CUDA
+    at::Tensor& y                   // [B,C,N], CUDA 
+);
